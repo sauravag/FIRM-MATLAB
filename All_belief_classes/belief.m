e437043ec99232a08641f17c3d8745a617ac405d
sauravag <saurav6@gmail.com>
2013-11-19 00:40:07 -0600

    Adding 3D ellipse drawing for 6 DOF belief

--- All_belief_classes/belief.m
+++ All_belief_classes/belief.m
@@ -9,9 +9,7 @@ classdef belief < belief_interface
             % The full list of properties for this function is:
             % 'RobotShape', 'RobotSize', 'TriaColor', 'color', 'HeadShape',
             % 'HeadSize', 'EllipseSpec'.
-            
-            ellipse_spec = '-r';  % Default value for "EllipseSpec" property.
-            ellipse_width = 2;
+                
             New_varargin={};
             
             for i = 1 : 2 : length(varargin)
@@ -25,39 +23,24 @@ classdef belief < belief_interface
                         New_varargin{end+1} = varargin{i+1}; %#ok<AGROW> % Note that in previous line the "end" itself is increased by 1. Thus, this line is correct.
                 end
             end
+            
             obj.est_mean = obj.est_mean.draw(New_varargin{:});
-           if ~isempty(ellipse_spec)
-%                 tmp=get(gca,'NextPlot'); hold on
-%                 obj.ellipse_handle = plotUncertainEllip2D(obj.est_cov(1:2,1:2),obj.est_mean.val(1:2),ellipse_spec, ellipse_width);
-%                 set(gca,'NextPlot',tmp);
-            end
+           
+            tmp=get(gca,'NextPlot'); hold on
+            m = obj.est_mean.val(1:3);
+            C = obj.est_cov(1:3,1:3);
+            sdwidth = 2;
+            obj.ellipse_handle = plot_gaussian_ellipsoid(m, C, sdwidth);
+            set(gca,'NextPlot',tmp);
         end
         function obj = draw_CovOnNominal(obj, nominal_state, varargin)
             % This function draws the belief. However, the estimation
             % covarinace is centered at the nominal state, provided by the
             % function caller.
-            
-            ellipse_spec = '-r';  % Default value for "EllipseSpec" property.
-            ellipse_width = 2;
-            New_varargin={};
-            
-            for i = 1 : 2 : length(varargin)
-                switch lower(varargin{i})
-                    case lower('EllipseSpec')
-                        ellipse_spec = varargin{i+1};
-                    case lower('EllipseWidth')
-                        ellipse_width = varargin{i+1};
-                    otherwise  % So, if the "varargin" is anything else, it is related to the plotting of "mean". So, we collect such inputs in "New_varargin" and pass them to the "obj.est_mean.draw".
-                        New_varargin{end+1} = varargin{i}; %#ok<AGROW>
-                        New_varargin{end+1} = varargin{i+1}; %#ok<AGROW> % Note that in previous line the "end" itself is increased by 1. Thus, this line is correct.
-                end
-            end
-            obj.est_mean = obj.est_mean.draw(New_varargin{:});
-           if ~isempty(ellipse_spec)
-%                 tmp=get(gca,'NextPlot'); hold on
-%                 obj.ellipse_handle = plotUncertainEllip2D(obj.est_cov(1:2,1:2),nominal_state.val(1:2),ellipse_spec, ellipse_width);
-%                 set(gca,'NextPlot',tmp);
-            end
+             m = nominal_state.val(1:3);
+             C = obj.est_cov(1:3,1:3);
+             sdwidth = 2;
+             obj.ellipse_handle = plot_gaussian_ellipsoid(m, C, sdwidth);
         end
         function obj = apply_differentiable_constraints(obj)
             obj.est_mean = obj.est_mean.apply_differentiable_constraints();
